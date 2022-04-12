2518346d91d30cfc3a085b954fcb68c04d7d736e
Vitaliy Zhiltsov <30690817+vzhgit@users.noreply.github.com>
2022-04-12 10:39:14 +0700

    switch to only one full backup + increments
    a date pattern for artifacts is the same as in TeamCity

--- teamcity_backup.sh
+++ teamcity_backup.sh
@@ -10,22 +10,25 @@ pathToArtifacts=${pathToTeamCity}/system/artifacts
 echo "Archiving Postgres DB ..."
 /home/$USER/TeamCity/bin/maintainDB.sh backup -A $pathToTeamCity --include-config --include-database --include-supplementary-data
 if [[ "$(date +%A)" == "Sunday" ]]; then
-  dt=$(date +'%Y.%m.%d')
+  dtFull=$(date +'%Y%m%d')
   zipOption=""
 else
-  dt=$(date +'%Y.%m.%d' -d "$(date +%u) days ago")
-  zipOption="-DF --out ${pathToBackup}/increment_for_${dt}.zip"
+  dt=$(date +'%Y%m%d')
+  dtFull=$(date +'%Y%m%d' -d "$(date +%u) days ago")
+  zipOption="-DF --out ${pathToBackup}${backupFile}_${dtFull}_increment_${dt}.zip"
 fi
-zipFile=${pathToBackup}${backupFile}_${dt}.zip
+zipFile=${pathToBackup}${backupFile}_${dtFull}.zip
 
 echo "Archiving $backupFile ..."
 zip -r $zipFile $pathToArtifacts $zipOption --quiet
 
-numberOfDays=14
-lastNdays=$(find ${pathToBackup}*.zip -type f -mtime -${numberOfDays} | wc -l)
-if [[ $lastNdays -ge $numberOfDays ]]; then
-  echo "Deleting old archives ..."
-  find ${pathToBackup}*.zip -type f -mtime +${numberOfDays} -exec rm -rf {} \;
-else
-  echo "Number of backups less than expected!"
+if [[ "$(date +%A)" == "Sunday" ]]; then
+  expectedBackups=2
+  numberOfArchives=$(find ${pathToBackup}*.zip -type f -name "*${dtFull}*" | wc -l)
+  if [[ $numberOfArchives -ge $expectedBackups ]]; then
+    echo "Deleting old archives ..."
+    find ${pathToBackup}*.zip -type f -not -name "*${dtFull}*" -exec rm -rf {} \;
+  else
+   echo "Number of backup files less than expected!"
+  fi
 fi
